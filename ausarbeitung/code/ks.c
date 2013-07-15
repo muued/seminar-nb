e0939dc52710f42006dba3cbf55899940bf5dea0
Christian Janssen <christian.janssen1@rwth-aachen.de>
2013-07-15 10:50:24 +0200

    Intro + old proposal

--- ausarbeitung/code/ks.c
+++ ausarbeitung/code/ks.c
@@ -7,7 +7,8 @@ int main(void){
 	int n = 20;
 	double x[]={1, 1.1, 1.2, 1.6, 1.7, 2.1, 2.1, 2.4, 2.4, 2.5, 2.6, 2.6, 2.6, 2.7, 2.8, 3, 3.3, 3.5, 3.8, 4.2};
 	Nag_Distributions dist = Nag_Normal;
-	double par[2]={3.5,1};
+	double par_franz[2]={3.5,1};
+	double par_paul[2]={3,0.7};
 	Nag_ParaEstimates estima = Nag_ParaSupplied;
 	Nag_TestStatistics dtype = Nag_TestStatisticsDAbs;
 	double d,z,p;
@@ -15,10 +16,20 @@ int main(void){
 	
 	INIT_FAIL(fail);
 	
-	nag_1_sample_ks_test(n,x,dist,par,estima,dtype,&d,&z,&p,&fail);
+	nag_1_sample_ks_test(n,x,dist,par_franz,estima,dtype,&d,&z,&p,&fail);
 	if (fail.code != NE_NOERROR){
-		printf("Error\n%s\n",fail.message);
+		printf("Error from nag_1_sample_ks_test (g08cbc).\n%s\n",fail.message);
 	}else{	
+		printf("Franz' values:\n");
+		printf("Statistic :%8.4f\n",d);
+		printf("p-value :%8.4f\n",p);
+	}
+	printf(" \n");
+	nag_1_sample_ks_test(n,x,dist,par_paul,estima,dtype,&d,&z,&p,&fail);
+	if (fail.code != NE_NOERROR){
+		printf("Error from nag_1_sample_ks_test (g08cbc).\n%s\n",fail.message);
+	}else{	
+		printf("Pauls values:\n");
 		printf("Statistic :%8.4f\n",d);
 		printf("p-value :%8.4f\n",p);
 	}
--- ausarbeitung/code/ks.r
+++ ausarbeitung/code/ks.r
@@ -1,5 +1,16 @@
+op <- options(warn = -1);
+cat("Values :\n");
 x <- c(1, 1.1, 1.2, 1.6, 1.7, 2.1, 2.1, 2.4, 2.4, 2.5, 2.6, 2.6, 2.6, 2.7, 2.8, 3, 3.3, 3.5, 3.8, 4.2);
+cat(x,"\n")
 y <- "pnorm";
 mean <- 3.5;
 sd <- 1;
-ks.test(x,y,mean,sd,alternative);
+cat("Franz' values:\n");
+result_franz <-ks.test(x,y,mean,sd,alternative="two.sided");
+cat("statistic ",result_franz$statistic," p-value: ",result_franz$p.value,"\n");
+mean <- 3;
+sd <-0.7;
+cat("Pauls values:\n");
+result_paul <- ks.test(x,y,mean,sd,alternative="two.sided")
+cat("statistic ",result_paul$statistic," p-value: ",result_paul$p.value,"\n");
+options(op);
